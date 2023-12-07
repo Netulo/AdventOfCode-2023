4aecacc8f54e92615f2363375b133ef3b0d27610
Netulo <matteu06@gmail.com>
2023-12-07 11:01:18 +0100

    Day 3 - complete (no optimalisation)

--- .vscode/3/Day3p2.cpp
+++ .vscode/3/Day3p2.cpp
@@ -7,10 +7,11 @@ using namespace std;
 
 int main()
 {
-    fstream myFile("test.txt");
+    fstream myFile("input.txt");
     string input;
     int sum = 0;
     vector<string> table;
+    vector<vector<int>> gears;
 
 
     getline(myFile, input);
@@ -22,34 +23,86 @@ int main()
     
     table.push_back(string(input.length(), '.'));
 
-    //Komentarz
+
 
     int digit = 0;
     for(int i = 1; i < table.size()-1; ++i)
     {
         for(int j = 0; j < table[i].length(); ++j)
         {
-            if(table[i][j] == '*')
+            if(isdigit(table[i][j]))
             {
-                for(int x = j-; x <= stop; ++x)
-                    {
-                        
-                        if(
-                            !isdigit(table[i+1][x]) && table[i+1][x] != '.' 
-                            || !isdigit(table[i][x]) && table[i][x] != '.'
-                            || !isdigit(table[i-1][x]) && table[i-1][x] != '.'
-                            )
-                        {
-                            //cout << digit << endl;
-                            sum += digit;
-                            break;
-                        }
-                    }      
+                digit = digit*10 + (table[i][j] - 0x30);
+                if(j != table[i].length()-1 && isdigit(table[i][j+1]))
+                    continue;
+                
             }
-            
+            if(digit > 0)
+            {
+                vector<int> gear;
+
+                int l = to_string(digit).length();
+                int x, stop, start;
+                    if(j-l < 0)
+                    {
+                        start = j-l+1;
+                        stop = j+1;
+                    }
+                    else if(j == table[i].length()-1)
+                    {
+                        start = j-l;
+                        stop = j;
+                    }
+                    else
+                    {
+                        start = j-l;
+                        stop = j+1;
+                    }
+                    
+                    for(int y = i-1; y <= i+1; ++y)
+                        {
+                            for(x = start; x <= stop; ++x)
+                            {
+                        
+                            if(table[y][x] == '*')
+                            {
+                                int z = 0;
+                                do
+                                {
+                                        if(gears.size() != 0 && gears[z][0] == x && gears[z][1] == y)
+                                        {
+                                            gears[z].push_back(digit);
+                                            break;
+                                        }
+                                        if(gears.size() == 0 || z == gears.size()-1)
+                                        {
+                                            gear.push_back(x);
+                                            gear.push_back(y);
+                                            gear.push_back(digit);
+                                            gears.push_back(gear);
+                                            gear.clear();
+                                            break;
+                                        }
+                                        z++;
+                                } while (z < gears.size());
+                            }
+                        }
+                    }
+                digit = 0;
+            }
         }
         digit = 0;
+    }
+
+    for(int i = 0; i < gears.size(); ++i)
+    {
+        if(gears[i].size()-2 == 2)
+        {
+        sum += gears[i][2]*gears[i][3];
+        }
     }
         
     printf("Sum is: %d", sum);
 }
+
+//87605697
